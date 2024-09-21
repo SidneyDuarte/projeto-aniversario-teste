221fc621113f886ca27e25f06420b2a0734111f9
Fernanda Rufato <nandaoliveira1985@terra.com.br>
2024-09-20 23:33:41 -0300

    finalizando o contador

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -584,7 +584,26 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"3cYfC":[function(require,module,exports) {
-console.log("ola mundo");
+const dataDoEvento = new Date("feb 19, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diasEmMs = 86400000;
+    const horasEmMs = 3600000;
+    const minutosEmMs = 60000;
+    const segundosEmMs = 1000;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % diasEmMs / horasEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % horasEmMs / minutosEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteOEvento % minutosEmMs / segundosEmMs);
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento expirado";
+    }
+}, 1000);
 
 },{}]},["8bDoD","3cYfC"], "3cYfC", "parcelRequire0fd0")
 
